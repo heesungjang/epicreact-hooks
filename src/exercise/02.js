ad1ed650c23afa890e54cacd4dce7d3cf8335fc8
Heesung jang <heesungj7@gmail.com>
2021-09-23 18:10:51 +0900

    Update 02.js

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,13 +4,15 @@
 import * as React from 'react'
 
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  console.log('rendering')
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') || initialName,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
@@ -27,7 +29,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Heesung" />
 }
 
 export default App
